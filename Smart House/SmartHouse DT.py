722f86f9b7d455b81f4d080080309c91f4ee759c
just-praize <chidera.ojor@gmail.com>
2025-05-15 02:22:09 +0200

    modified code

--- Smart House/SmartHouse DT.py
+++ Smart House/SmartHouse DT.py
@@ -1,3 +1,4 @@
+import random
 class SmartHomeDigitalTwin:
     def __init__(self):
         # Initialize the initial states of devices
@@ -7,59 +8,51 @@ class SmartHomeDigitalTwin:
         self.speaker_status = "on"
 
     def get_light_status(self):
-        self.get_light_status(self.light_status)
-        print(f"Light is {self.light_status}")
+        if self.light_status:
+            print(f"Light is {self.light_status}")
 
     def toggle_light(self):
         self.light_status = "on" if self.light_status == "off" else "off"
         print(f"Light is turned {self.light_status}")
 
-    def get_thermostat_status(self):
-        self.get_thermostat_status(self.thermostat_temperature)
-        print(f"Thermostat is set to {self.thermostat_temperature}")
-
     def set_temperature(self, temperature):
         self.temperature = temperature
         if self.temperature < 10 or self.temperature > 30:
-            print(f"Error: Value is out of range")
+            print(f"Warning: Extreme Temperature")
         print(f"Temperature set to {self.temperature}")
 
+    def get_thermostat_status(self):
+        if self.thermostat_temperature:
+            print(f"Thermostat is set to {self.thermostat_temperature}")
+
     def get_door_lock_status(self):
-        self.get_door_lock_status(self.door_lock_status)
-        print(f"Door is {self.door_lock_status}")
+        if self.door_lock_status:
+            print(f"Door is {self.door_lock_status}")
 
     def toggle_door_lock(self):
-        self.toggle_door_lock = (
-            "locked" if self.toggle_door_lock == "unlocked" else "unlocked"
-        )
+        self.door_lock_status = "unlocked" if self.door_lock_status == "locked" else "locked"
+        print(f"Door is now {self.door_lock_status}")
 
     # Additional Device (Smart Speaker) Implementation
     def get_speaker_status(self):
-        self.get_speaker_status(self.speaker_status)
-        print(f"Speaker is {self.speaker_status}")
+        if self.speaker_status:
+            print(f"Speaker is {self.speaker_status}")
 
     def toggle_speaker(self):
-        self.toggle_speaker = "on" if self.toggle_speaker == "off" else "off"
-        print(f"Speaker is {self.toggle_speaker}")
+        self.speaker_status = "off" if self.speaker_status == "on" else "on"
+        print(f"Speaker is {self.speaker_status}")
 
-    def simulate_user_interractions(self):
-        print(self.get_light_status())
-        self.toggle_light()
-        print(self.get_light_status())
-
-        print(self.get_thermostat_status())
-        self.set_temperature(25)
-        print(self.get_thermostat_status())
-
-        print(self.get_door_lock_status())
-        self.toggle_door_lock()
-        print(self.get_door_lock_status())
-
-        print(self.get_speaker_status())
-        self.toggle_speaker()
-        print(self.get_speaker_status())
-
+    def simulate_user_interractions(self, no_of_interractions):
+        for i in range(no_of_interractions):
+            self.get_light_status()
+            self.toggle_light()
+            self.set_temperature(temperature=random.randint(9, 32))
+            self.get_thermostat_status()
+            self.get_door_lock_status()
+            self.toggle_door_lock()
+            self.get_speaker_status()
+            self.toggle_speaker()
 
 SHDT = SmartHomeDigitalTwin()
 
-SHDT.simulate_user_interractions()
+SHDT.simulate_user_interractions(4)
